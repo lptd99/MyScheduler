b71d3a85be4e9da6cb9b7487f4c675312fefdc4c
Tyo Drak <lptd99@gmail.com>
2023-03-12 10:55:03 -0400

    Extend data input, 'save' all, radios not 'saving'.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import UserForm from './components/UserForm';
+import { initializeApp } from "firebase/app";
 
 function App() {
 
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
+
+  const app = initializeApp(firebaseConfig);
+
   return (
     <div className="App">
-      <UserForm></UserForm>
+      <UserForm db={app}></UserForm>
     </div>
   );
 }
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
-import ENV from ".env"
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore"; 
 
-function UserForm() {
+function UserForm( {appDB} ) {
 
     const [inputUsername, setInputUsername] = useState('');
     const [inputPassword, setInputPassword] = useState('');
+    const [radioGender, setRadioGender] = useState('');
+    const [taLikes, setTaLikes] = useState([]);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [gender, setGender] = useState('');
+    const [likes, setLikes] = useState('');
     const [saved, setSaved] = useState(false);
 
-    const firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID
-      };
-
+    const db = getFirestore(appDB);
 
     const inputUsernameChange = (event) => {
     setInputUsername(event.target.value);
@@ -27,29 +24,77 @@ function UserForm() {
     }
 
     const inputPasswordChange = (event) => {
-    setInputPassword(event.target.value);
-    if(saved){
-        setSaved(false);
-    }
+        setInputPassword(event.target.value);
+        if(saved){
+            setSaved(false);
+        }
     }
 
-    const saveButton = () => {
-    setUsername(inputUsername);
-    setPassword(inputPassword);
-    setSaved(true);
+    const radioGenderChange = (event) => {
+        setRadioGender(event.target.value);
+        if(saved){
+            setSaved(false);
+        }
+    }
+
+    const taLikesChange = (event) => {
+        setTaLikes(event.target.value);
+        if(saved){
+            setSaved(false);
+        }
+    }
+
+    function saveButton() {
+    //     try {
+    //         const docRef = await addDoc(collection(db, "users"), {
+    //           username: username,
+    //           password: password,
+    //           gender: gender,
+    //           likes: likes
+    //         });
+    //         console.log("Document written with ID: ", docRef.id);
+    //       } catch (e) {
+    //         console.error("Error adding document: ", e);
+    //       }
+        setUsername(inputUsername);
+        setPassword(inputPassword);
+        setGender(radioGender);
+        setLikes(taLikes.split(','));
+        setSaved(true);
     }
 
     return (
         <div>
             {saved ? <h3>Saved!</h3> : <h3> Not saved.</h3> }
+            <br/>
             <input type='username' placeholder='Username here' onChange={inputUsernameChange} value={inputUsername} id='inputUsername' name='inputUsername'/>
             <br/>
+            <br/>
             <input type='password' placeholder='Password shhhh' onChange={inputPasswordChange} value={inputPassword} id='inputPassword' name='inputPassword'/>
+            {/* <br/>
+            <br/>
+            <label>Gender:</label>
+            <br/>
+            <input type='radio' onChange={radioGenderChange} value={radioGender} id='radioGenderMale' name='radioGender'/>
+            <label for='radioGender'>Male</label>
+            <input type='radio' onChange={radioGenderChange} value={radioGender} id='radioGenderFemale' name='radioGender'/>
+            <label for='radioGender'>Female</label>
+            <input type='radio' onChange={radioGenderChange} value={radioGender} id='radioGenderPNS' name='radioGender'/>
+            <label for='radioGender'>Prefer not to specify</label> */}
+            <br/>
+            <br/>
+            <label>Likes (comma separated)</label>
+            <br/>
+            <textarea rows={5} cols={10} placeholder='Things, you, like' onChange={taLikesChange} value={taLikes} id='taLikes' name='taLikes'/>
             <br/>
             <button onClick={saveButton}>Save?</button>
             <br/>
-            <h3>Username: {username}</h3>
-            <h3 color='white'>Password: {password}</h3>
+            <br/>
+            <br/>
+            <h3>Username: {inputUsername + ' ' + username}</h3>
+            <h3>Password: {inputPassword + ' ' + password}</h3>
+            <h3>Gender: {radioGender + ' ' + gender}</h3>
+            <h3>Likes: {taLikes + ' ' + likes}</h3>
         </div>
     )
 }
